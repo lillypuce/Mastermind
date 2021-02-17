5173f5f7d02e8d801ace331e16801647283c0ce5
lahamzio <lahamzio@pcalb-eb0108.univ-jfc.fr>
2021-02-17 11:12:37 +0100

    test

--- src/VuePropositions.java
+++ src/VuePropositions.java
@@ -1,5 +1,38 @@
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.WindowEvent;
 
-public class VuePropositions {
+public class VuePropositions extends Canvas{
+	Modèle modl;
+	Frame mainFrame;
+	
+	
+	public VuePropositions(){
+		
+		mainFrame=new Frame("test");
+		mainFrame.setSize(500,500);
+		
+		mainFrame.setBackground(Color.GRAY);
+		mainFrame.setVisible(true);
+		
+		
+		
+		
+	}
+	
+	public void cercle(Graphics g) {
+		
+		g.drawOval(10, 10, 10, 10);
+		g.setColor(Color.BLACK);
+		
+	}
+	
+	
+	
+	
+	
 
 
 }
