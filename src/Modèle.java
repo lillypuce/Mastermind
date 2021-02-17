9e00f3b285a0541f65bfb004efbae5f5783b9eec
Pauline Castelbou <pcastelb@pcalb-mm0000.univ-jfc.fr>
2021-02-17 11:20:50 +0100

    Ajout Rangée

--- src/Modèle.java
+++ src/Modèle.java
@@ -1,4 +1,10 @@
+import java.awt.Color;
 
 public class Modèle {
+	
+	Color[] COULEURS = {Color.yellow,Color.green,Color.blue,Color.magenta,Color.red,Color.orange,Color.white,Color.black};
+	Integer N_TENTATIVES = 10;
+	static Integer DIFFICULTE = 4;
+	String[] Etat = {"EN_COURS","GAGNE","PERDU"};
 
 }
--- src/Rangée.java
+++ src/Rangée.java
@@ -1,4 +1,20 @@
+import java.awt.Color;
 
 public class Rangée {
+	
+	int[] jetons;
+	int indiceJeton;
+	
+	public class Résultat {
+		int blancs;
+		int noirs;
+	}
+	
+	Résultat résultat;
+	
+	public Rangée() {
+		this.jetons = new int[Modèle.DIFFICULTE];
+		this.indiceJeton = 0;
+	}
 
 }
