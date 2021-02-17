3919446fbac6f74f3026a75abd5cb388b7353017
Pauline Castelbou <pcastelb@pcalb-mm0000.univ-jfc.fr>
2021-02-17 11:31:44 +0100

    Modif modèle

--- src/Modèle.java
+++ src/Modèle.java
@@ -3,8 +3,23 @@ import java.awt.Color;
 public class Modèle {
 	
 	Color[] COULEURS = {Color.yellow,Color.green,Color.blue,Color.magenta,Color.red,Color.orange,Color.white,Color.black};
-	Integer N_TENTATIVES = 10;
+	static Integer N_TENTATIVES = 10;
 	static Integer DIFFICULTE = 4;
-	String[] Etat = {"EN_COURS","GAGNE","PERDU"};
+	enum Etat {EN_COURS,GAGNE,PERDU};
+	
+	Etat état;
+	Rangée combinaison;
+	Rangée[] propositions;
+	Integer tentative;
+	
+	public Modèle() {
+		this.combinaison = new Rangée();
+		
+		this.propositions = new Rangée[Modèle.N_TENTATIVES];
+		
+		this.tentative=  0;
+		
+		this.état = Etat.EN_COURS;
+	}
 
 }
