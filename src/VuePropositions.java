bff4e126a52d6d45af52eb507f7dbfa1724da1ed
Leila Ahamziou Rechou <lahamzio@pcalb-mm0000.univ-jfc.fr>
2021-02-17 11:44:05 +0100

    idk

--- src/VuePropositions.java
+++ src/VuePropositions.java
@@ -1,5 +1,6 @@
 import java.awt.Canvas;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Graphics;
 import java.awt.event.WindowEvent;
@@ -7,19 +8,16 @@ import java.awt.event.WindowEvent;
 public class VuePropositions extends Canvas{
 	Modèle modl;
 	Frame mainFrame;
-	
+	Dimension d;
 	
 	
 	public VuePropositions(){
 		super();
 		this.modl=new Modèle();
+		this.d=new Dimension(100,100);
+		this.setSize(d);
 		
-		mainFrame=new Frame("test");
-		mainFrame.setSize(500,500);
 		
-		mainFrame.setBackground(Color.GRAY);
-		mainFrame.pack();
-		mainFrame.setVisible(true);
 		
 		
 		
@@ -27,18 +25,26 @@ public class VuePropositions extends Canvas{
 	}
 	
 	public void cercle(Graphics g) {
+		g.setColor(Color.lightGray);
 		
-		g.drawOval(10, 10, 10, 10);
-		g.setColor(Color.BLACK);
 		
-		g.drawOval(20, 10, 10, 10);
-		g.setColor(Color.BLACK);
+		int x=10;
+		for(int i=0;i<4;i++) {
+			g.drawOval(x+i*10, x, 10, 10);
+			g.setColor(Color.BLACK);
+		}
 		
-		g.drawOval(30, 10, 10, 10);
-		g.setColor(Color.BLACK);
+		//g.drawOval(10, 10, 10, 10);
+		//g.setColor(Color.BLACK);
 		
-		g.drawOval(40, 10, 10, 10);
-		g.setColor(Color.BLACK);
+	//	g.drawOval(20, 10, 10, 10);
+		//g.setColor(Color.BLACK);
+		
+		//g.drawOval(30, 10, 10, 10);
+	//	g.setColor(Color.BLACK);
+		
+	//	g.drawOval(40, 10, 10, 10);
+		//g.setColor(Color.BLACK);
 		
 	}
 	
