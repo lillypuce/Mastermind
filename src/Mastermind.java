daa9a969b3f3eb7db577b52eec798350ab9ba809
Pauline Castelbou <pcastelb@pcalb-mm0000.univ-jfc.fr>
2021-02-17 10:41:52 +0100

    Réorganisation

---
+++ src/Mastermind.java
@@ -0,0 +1,10 @@
+import java.awt.Frame;
+
+public class Mastermind extends Frame {
+
+	public static void main(String[] args) {
+		new Mastermind();
+
+	}
+
+}
--- src/VueClavier.java
+++ src/VueClavier.java
@@ -1,9 +1,6 @@
 
 public class VueClavier {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	
 
 }
